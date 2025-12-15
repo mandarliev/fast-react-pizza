147e6b741aff40e2145aa2a46cdf2040a99529f1
Dimitar <dimitar.mandarliev@gmail.com>
2025-12-15 14:08:20 +0200

    Building the App Layout

---
+++ src/UI/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Fast React Pizza Co.</Link>
+      <p>Dimitar</p>
+    </header>
+  );
+}
+
+export default Header;
